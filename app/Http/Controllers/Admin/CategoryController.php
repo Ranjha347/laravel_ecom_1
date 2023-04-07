127d07b305b173af894e704c303dcf4efa08168b
Ranjha347 <uir347@gmail.com>
2023-04-07 16:00:33 +0200

    Create Category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CategoryformRequest;
 
 class CategoryController extends Controller
 {
@@ -13,5 +15,36 @@ class CategoryController extends Controller
 
     public function create(){
         return view("admin.category.create");
+    }
+
+    public function store(CategoryformRequest $request){
+        $validated = $request->validated();
+
+        $category = new Category;
+
+        $category->name = $validated['name'];
+        $category->slug = $validated['slug'];
+        $category->description = $validated['description'];
+
+        if($request->hasFile('image')){
+            $file = $request->file("image");
+            $ext = $file->getClientOriginalExtension();
+
+            $filename = time(). ".".$ext;
+
+            $file->move("uploads/category/",$filename);
+
+            $category->image = $filename;
+        }
+        $category->meta_title = $validated['meta_title'];
+        $category->meta_keyword = $validated['meta_keyword'];
+        $category->meta_description = $validated['description'];
+
+        $category->status = $request->status == TRUE ? "1" : "2";
+
+        $category->save();
+
+        return redirect("/admin/category")->with("message","Category Added Successfully.");
+        
     }
 }
